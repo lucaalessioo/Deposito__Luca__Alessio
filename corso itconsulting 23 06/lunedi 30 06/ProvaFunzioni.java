ed9bed83ad4070501560cb24ed9f2a1e6c155f20
lucaalessioo <lucaalessioo@hotmail.com>
2025-06-30 10:32:18 +0200

    ProvaClassi

--- corso itconsulting 23 06/lunedi 30 06/ProvaFunzioni.java
+++ corso itconsulting 23 06/lunedi 30 06/ProvaFunzioni.java
@@ -1,16 +1,31 @@
-public class ProvaFunzioni {
+class UtilitaMatematica {
 
+   static int quadrato(int num) { //metodo statico
+       return num * num;
+   }
+   int cubo(int num) {
+       return num * num * num ;
+   }
+}
+
+public class ProvaFunzioni {
+    
     public static void main(String[] args) {
         
-       int risultato = (fattoriale(5));
+       /*  int risultato = (fattoriale(5));
         System.out.println(risultato);
     }
-     static int fattoriale(int n) {
+    static int fattoriale(int n) {
         if(n==1) {
             return 1;
         }
         return n * fattoriale(n-1);
-    }
+    }*/
+    
+    System.out.println(UtilitaMatematica.quadrato(5));  // nessun oggetto necessario
+    UtilitaMatematica obj = new UtilitaMatematica();
+    System.out.println(obj.cubo(5));   // Oggetto nescessario
 
+    }
     
 }
