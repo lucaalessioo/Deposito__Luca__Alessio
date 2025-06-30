c2f863a907d738d8db86c95221c3fd75bccdf9b3
lucaalessioo <lucaalessioo@hotmail.com>
2025-06-30 15:22:56 +0200

    Update ApplicationCalcolatrice.java

--- corso itconsulting 23 06/lunedi 30 06/ApplicationCalcolatrice.java
+++ corso itconsulting 23 06/lunedi 30 06/ApplicationCalcolatrice.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class ApplicationCalcolatrice {
     public static void main(String[] args) {
@@ -7,6 +8,13 @@ public class ApplicationCalcolatrice {
         
         int risultato = calcolatrice.somma(10, 5);
         System.out.println(risultato);
+
+        ArrayList<Auto> autoList = new ArrayList<>();
+        autoList.add(new Auto("Tesla", 2023));
+        autoList.add(new Auto("Ford", 2020));
+        for (Auto auto : autoList) {
+        System.out.println(auto.marca + " - " + auto.anno);
+        }
     }
 }
 
@@ -19,4 +27,15 @@ class Calcolatrice {
     void saluta() {
         System.out.println("Ciao");
     }
+}
+
+class Auto {
+    String marca;
+    int anno;
+
+    Auto(String marca , int anno) {
+        this.marca = marca;
+        this.anno = anno;
+    }
+
 }
