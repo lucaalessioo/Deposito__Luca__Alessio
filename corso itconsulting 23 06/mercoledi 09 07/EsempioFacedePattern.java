562c36555b9fc59b826b3460436196d89709cd7f
lucaalessioo <lucaalessioo@hotmail.com>
2025-07-09 17:32:32 +0200

    esempio facede

---
+++ corso itconsulting 23 06/mercoledi 09 07/EsempioFacedePattern.java
@@ -0,0 +1,31 @@
+// Sottosistema
+class SubsystemA {
+    void operationA() {
+        System.out.println("Operazione A del sottosistema A");
+    }
+}
+
+class SubsystemB {
+    void operationB() {
+        System.out.println("Operazione B del sottosistema B");
+    }
+}
+
+// Facade
+class Facade {
+    private SubsystemA a = new SubsystemA();
+    private SubsystemB b = new SubsystemB();
+
+    public void operazioneUnificata() {
+        a.operationA();
+        b.operationB();
+    }
+}
+
+// Client
+public class EsempioFacedePattern {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.operazioneUnificata();
+    }
+}
